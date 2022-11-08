386b53eb745a95bcfe0ae5c3f83ff9b8ff3c2744
devnik <lamigo.nik@gmail.com>
2022-11-08 13:35:49 +0800

    ASP.NET & Angular Course - Section 11: Photo Upload

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -17,7 +17,7 @@ export class MembersService {
   getMembers() {
     if (this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map((members: Member[]) => {
+      map(members => {
         this.members = members;
         return members;
       })
@@ -37,5 +37,13 @@ export class MembersService {
         this.members[index] = member;
       })
     )
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
